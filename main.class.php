402a61bf232e5a74db53bface8a139ab76b71e56
Daniel Baddeley <daniel.baddeley@techdata.com>
2015-03-05 15:07:31 -0500

    Added {Auto}Loading For Classes Used By CoffeeBean

--- main.class.php
+++ main.class.php
@@ -4,11 +4,17 @@ namespace CoffeeBean;
 class Main {
 	private $WORKING_DIR = __DIR__;
 	private $CONFIG_NAME = 'config.ini';
+	private $MAIN_CLASS  = 'main.class.php';
 	public $config;
 
 	public function __construct(){
 		$this->getConfig();
 		$this->debug($this->config);
+		/**
+		 *  Include Necessary Application Classes
+		 */
+		 //spl_autoload_register('manualLoadClasses'); #Should Be Deprecated In Favour Of Dynamic Inclusion.
+		 spl_autoload_register('autoLoadClasses');
 	}
 	
 	private function getConfig(){
@@ -26,5 +32,35 @@ class Main {
 
 		$message = ($message != null) ? $message : $trace_location;
 		return error_log($message . json_encode($arg) . "\n",3,$this->WORKING_DIR.'/debug.log');
+	}
+	
+	/**
+	 *  @deprecated: Used to load classes used by CoffeeBean
+	 */
+	public function manualLoadClasses(){
+		/**
+		 *  PayPal API/SDK Integration
+		 */
+		include_once $this->WORKING_DIR.'/PayPal/'.$this->MAIN_CLASS;
+		
+		/**
+		 *  Module For Helping With Requests: POST | GET | REQUEST | FILE | RAW
+		 */
+		include_once $this->WORKING_DIR.'/RequestHelper/'.$this->MAIN_CLASS;
+		
+		/**
+		 *  Module For Misc. Helpful Tools.
+		 *  @Browser : Sub-Module For [BagaTrix] Used To Determine End-User Browser Information
+		 */
+		include_once $this->WORKING_DIR.'/BagaTrix/'.$this->MAIN_CLASS;
+		include_once $this->WORKING_DIR.'/BagaTrix/Browser/'.$this->MAIN_CLASS;
+	}
+	/**
+	 *  @autoloader : used to {auto}load classes used by CoffeBeen
+	 */
+	public function autoLoadClasses() {
+		foreach($this->config['EnabledModules'] as $key => $value) {
+			($key != true) ? continue : include_once $this->WORKING_DIR.'/'.$key.'/'.$this->MAIN_CLASS;
+		}
 	}
 }
